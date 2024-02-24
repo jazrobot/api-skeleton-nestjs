950b721fda879ed8c4ad1eafbfd536053df0cc36
jazrobot <jazz_yoso@hotmail.com>
2024-02-24 15:07:47 -0600

    Update PORT default value to 5000

--- src/modules/env/env.schema.ts
+++ src/modules/env/env.schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  PORT: z.coerce.number().default(3000),
+  PORT: z.coerce.number().default(5000),
   DATABASE_URL: z.string().url(),
   JWT_SECRET_KEY: z.string(),
 });
